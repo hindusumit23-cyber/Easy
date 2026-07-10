04d74dcba75d7a3d2f2dfdf5dc4e8d108bc42534
Sumit <hindusumit23@gmail.com>
2026-07-10 13:27:26 +0530

    Solving Cyclic Sort Problems

---
+++ leet448.java
@@ -0,0 +1,23 @@
+class leet448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while(i<nums.length){
+            int correct = nums[i] - 1;
+            if(nums[i] != nums[correct]){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+        ArrayList <Integer> ans = new ArrayList<>();
+        for(int j = 0; j<nums.length; j++){
+        if(nums[j] != j+1){
+            ans.add(j+1);
+        }
+        }
+        return ans;
+    }
+}
